63445e893aff4e6ef1a07d4b9d1f34e9062fcf4c
JoyRume <joy.rume@student.moringaschool.com>
2024-05-09 14:15:23 +0300

    added two routes

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import db, User, Like, BlogPost, Community, Expert, Message, Comment
@@ -13,6 +13,20 @@ CORS(app)
 @app.route('/')
 def index():
     return '<h1>Project Server successfully created</h1>'
+
+# get all users 
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    users = User.query.all()
+    user_list = [user.to_dict() for user in users]
+    return jsonify(user_list)
+
+# get all BlogPost
+@app.route('/blogposts')
+def get_all_blogposts():
+    blogposts = BlogPost.query.all()
+    blogposts_list = [blogposts.to_dict() for blogpost in blogposts]
+    return jsonify(blogposts_list)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)

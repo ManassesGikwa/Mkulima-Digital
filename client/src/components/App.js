4360e41de504693e0562e708a14e9c7b9ac69cd4
Marvin <okongomarvin971@gmail.com>
2024-05-11 00:39:42 +0300

    stylings

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,7 @@ function App(){
   return(
   <div className="main">
     <NavBar/>
+    
     <Footer />
   </div>
   )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -160,7 +160,6 @@ const Home = () => {
     </div>
     </div>
     </div>
-    </div>
   );
 };
 

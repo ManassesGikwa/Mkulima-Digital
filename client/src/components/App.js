9999352211109b7c7de23304efa7768acda94d16
JoyRume <joy.rume@student.moringaschool.com>
2024-05-17 14:52:14 +0300

    updated the community details page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,26 +9,29 @@ import Careers from '../pages/Careers';
 import Community from "../pages/Community";
 import LoginForm from "../pages/loginForm";
 import SignupForm from "../pages/signupForm";
-import CommunityDetails from "../pages/CommunityDetails"; // Import the CommunityDetails component
+import CommunityDetails from "../pages/CommunityDetails"; 
+import EditCommunity from '../pages/EditCommunity';
 
 function App() {
   const [registered, setRegistered] = useState(false);
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={registered ? <Navigate to="/home" /> : <SignupForm setRegistered={setRegistered} />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/signup" element={<SignupForm setRegistered={setRegistered} />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/finance" element={<Finance />} /> 
-        <Route path="/careers" element={<Careers />} /> 
-        <Route path="/community" element={<Community />} /> 
-        <Route path="/community/:id" element={<CommunityDetails />} /> {/* Add this route */}
-        <Route path="/blogs" element={<Posts />} /> 
-        <Route path="/solutions" element={<Solutions />} />
-        <Route path="*" element={<Navigate to="/home" />} />
-      </Routes>
+    
+        <Routes>
+          <Route path="/" element={registered ? <Navigate to="/home" /> : <SignupForm setRegistered={setRegistered} />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/signup" element={<SignupForm setRegistered={setRegistered} />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/finance" element={<Finance />} /> 
+          <Route path="/careers" element={<Careers />} /> 
+          <Route path="/community" element={<Community />} /> 
+          <Route path="/community/:id" element={<CommunityDetails />} />
+          <Route path="/community/:id/edit" element={<EditCommunity/>} />
+          <Route path="/blogs" element={<Posts />} /> 
+          <Route path="/solutions" element={<Solutions />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
     </div>
   );
 }
